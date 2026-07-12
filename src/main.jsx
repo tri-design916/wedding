a2e6b333af03766801ad9da6ed2f6d1a6295d40f
서연주 <seoyeonju@seoyeonjuui-MacBookPro.local>
2026-07-12 18:31:28 +0900

    Block pinch-zoom gestures via JS/CSS (iOS Safari ignores viewport user-scalable)

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,9 @@ import { createRoot } from "react-dom/client";
 import { App } from "./App.jsx";
 import "./styles.css";
 
+document.addEventListener("gesturestart", e => e.preventDefault());
+document.addEventListener("touchmove", e => { if (e.touches.length > 1) e.preventDefault() }, { passive: false });
+
 createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
